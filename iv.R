de61af72228e847e20bd1841885a743690411537
Mario Pasquato <mario.pasquato@gmail.com>
2023-03-06 13:27:29 -0500

    diagnostic plots

--- iv.R
+++ iv.R
@@ -27,5 +27,25 @@ bootstrap_iv <- function(i)
     ivreg(y ~ x | z)$coefficients[2]
 }
 
+pdf("bootstrap.pdf")
 plot(density(sapply(1:1000, bootstrap_ols)), xlim = c(0,5), xlab = "regression coefficient", ylab = "frequency", main = "")
 lines(density(sapply(1:1000, bootstrap_iv)), col = "#C44218")
+
+predicted_values <- lm(d$log.M_sph.M_Sun. ~ d$log.M_DM.M_Sun.)$fitted.values
+secondstage <- lm(d$log.M_BH.M_Sun. ~ predicted_values)
+summary(secondstage)
+
+pdf("naive_OLS.pdf")
+plot(d$log.M_sph.M_Sun., d$log.M_BH.M_Sun., ylab = expression(M[BH]), xlab = expression(M[bulge]), main = "Naive OLS regression", pch = 16)
+#abline(mean(d$log.M_BH.M_Sun.)-mean(d$log.M_sph.M_Sun.),1, col = "#A0A0A0")
+#abline(mean(d$log.M_BH.M_Sun.)-2*mean(d$log.M_sph.M_Sun.),2, col = "#C44218")
+abline(naive_OLS$coefficients)
+abline(secondstage$coefficients, col = "#C44218")
+
+pdf("second_stage.pdf")
+plot(predicted_values, d$log.M_BH.M_Sun., ylab = expression(M[BH]), xlab = expression("Predicted "*M[bulge]), main = "Second stage IV regression", pch = 16)
+#abline(mean(d$log.M_BH.M_Sun.)-2*mean(predicted_values),2, col = "#C44218")
+#abline(mean(d$log.M_BH.M_Sun.)-mean(predicted_values),1, col = "#A0A0A0")
+abline(naive_OLS$coefficients)
+abline(secondstage$coefficients, col = "#C44218")
+
